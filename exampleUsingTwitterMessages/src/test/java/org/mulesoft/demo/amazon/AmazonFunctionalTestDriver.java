b4e2bed4904002409dc68e38647232ae1e6a2718
Gaston Ponti <gastonponti@zaubersoftware.com>
2011-11-21 13:25:06 -0300

    Example using twitter.

---
+++ exampleUsingTwitterMessages/src/test/java/org/mulesoft/demo/amazon/AmazonFunctionalTestDriver.java
@@ -0,0 +1,46 @@
+/**
+ * Mule Amazon Connector
+ *
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mulesoft.demo.amazon;
+
+import org.junit.Test;
+import org.mule.api.MuleEvent;
+import org.mule.construct.Flow;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+
+/**
+ * @author Gaston Ponti
+ * @since Nov 16, 2011
+ */
+
+public class AmazonFunctionalTestDriver extends FunctionalTestCase
+{
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "mule-config.xml";
+    }
+    
+    @Test
+    public void testExample() throws Exception
+    {
+        MuleEvent process = lookupFlowConstruct("producer").process(getTestEvent(""));
+        System.out.println(process.getMessage().getPayload());
+    }
+
+    private Flow lookupFlowConstruct(final String name)
+    {
+        return (Flow) muleContext.getRegistry().lookupFlowConstruct(name);
+    }
+
+}
+
